4f21ee9308baa56d31714cd9b62cbfe5d1ade732
Jorgeney Santos <121130654+23Jorge@users.noreply.github.com>
2024-09-28 11:05:28 -0400

    upgradeGaditaEstamparia

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ var menuIcon = document.querySelector('.menu-icon');
 var ul = document.querySelector('.ul');
 
 menuIcon.addEventListener('click',()=>{
-    if (ul.classList.contains('ativo')) {
-        ul.classList.remove('ativo');        
-    }else{
-        ul.classList.add('ativo')
-    }
+   if(ul.classList.contains('ativo')) {
+    ul.classList.remove('ativo');
+
+   }else{
+    ul.classList.add('ativo');
+   }
 })
